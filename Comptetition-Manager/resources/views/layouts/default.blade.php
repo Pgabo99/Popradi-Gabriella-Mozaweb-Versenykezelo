256c5dcf5d1195a52cd3b87534e9528b9b6c47d7
Poprádi Gabriella <h268517@stud.u-szeged.hu>
2025-01-16 21:53:37 +0100

    The menu is Role based

--- Comptetition-Manager/resources/views/layouts/default.blade.php
+++ Comptetition-Manager/resources/views/layouts/default.blade.php
@@ -4,29 +4,31 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--Token for the CRUD methods -->
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   @yield("header")
   <title>@yield("title", "Versenykezelő")</title>
+  <!-- Bootstrap-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- DataTables-->
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.min.css">
+  <!-- jQuery-->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <!-- SweetAlert-->
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-  <script>
-    function confirmDelete(formId) {
-      if (confirm("Biztos, hogy törölni akarod?"))
-        document.getElementById(formId).submit();
-    }
-  </script>
+
 </head>
 
 <body>
   @include('layouts.menu')
 
   @yield("content")
+  <!-- Bootstrap-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
+  <!-- DataTables-->
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/2.2.1/js/dataTables.min.js"></script>
 </body>
 
